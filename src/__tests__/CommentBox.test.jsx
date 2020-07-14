569b8eb9c4c2284252bfb9fd8c99bd6dddc1072e
Languiller <Daniel.Languiller@team.telstra.com>
2020-07-14 16:31:30 +1000

    comment box actions

--- src/__tests__/CommentBox.test.jsx
+++ src/__tests__/CommentBox.test.jsx
@@ -16,3 +16,27 @@ it("has a textarea and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
+
+describe("text area:", () => {
+  beforeEach(() => {
+    // simulating an onchange event
+    wrapped.find("textarea").simulate("change", {
+      target: {
+        value: "new comment",
+      },
+    });
+    wrapped.update();
+  });
+
+  it("users can type in textarea", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
+
+  it("when form is submitted, textarea gets emptied", () => {
+    // simulate a "submit" form event
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    // check that wrapped is empty now.
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
+});
